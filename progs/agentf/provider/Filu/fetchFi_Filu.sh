331ac41204d014b650f2328b310dff0ba78fdf0e
loh.tar <loh.tar@googlemail.com>
2010-11-08 18:20:10 +0100

    Updated to fit more doc/import-file-format.txt

--- progs/agentf/provider/Filu/fetchFi_Filu.sh
+++ progs/agentf/provider/Filu/fetchFi_Filu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "HEADER=MySymbol;Name;Type;Native Market;Provider-Symbol-Market;Provider-Symbol-Market"
+echo "[Header]RefSymbol;Name;Type;Provider0;Symbol0;Market0;Provider1;Symbol1;Market1"
 cat all_FIs_Filu.csv |grep -i -E $1
