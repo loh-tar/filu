8c82145a7bc69e59f2c8e621b2d476db4676e388
loh.tar <loh.tar@googlemail.com>
2010-11-24 21:33:46 +0100

    Fix Header to new needs

--- progs/agentf/provider/Filu/fetchIdx_Filu.sh
+++ progs/agentf/provider/Filu/fetchIdx_Filu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #cd /usr/local/lib/Filu/provider/Filu/
-echo "HEADER=Name;iSymbol"
+echo "[Header]Name;iSymbol"
 grep -h --include=Index_*.csv  IndexName * | sed  s/*IndexName=//
 #grep -h --include=Index_*.csv  IndexName * |grep -h -i $1 | sed  s/*IndexName=//
