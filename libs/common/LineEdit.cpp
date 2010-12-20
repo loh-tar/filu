3c0831bcd01e8c0504edc84382b01c3447dc4502
loh.tar <loh.tar@googlemail.com>
2010-12-20 10:37:25 +0100

    New class LineEdit with an extra clear button inside
    Thanks to Girish Ramakrishnan
    http://labs.qt.nokia.com/2007/06/06/lineedit-with-a-clear-button

---
+++ libs/common/LineEdit.cpp
@@ -0,0 +1,65 @@
+//
+//   This file is part of Filu.
+//
+//   Copyright (C) 2007, 2010  loh.tar@googlemail.com
+//
+//   Filu is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   Filu is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with Filu. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "LineEdit.h"
+
+#include <QToolButton>
+#include <QStyle>
+
+LineEdit::LineEdit(QWidget *parent)
+        : QLineEdit(parent)
+{
+  mClearBtn = new QToolButton(this);
+  //QIcon icon = QIcon::fromTheme("edit-clear-locationbar-rtl");
+  //mClearBtn->setIcon(icon);
+  mClearBtn->setArrowType(Qt::LeftArrow);
+  mClearBtn->setCursor(Qt::ArrowCursor);
+  mClearBtn->setStyleSheet("QToolButton { border: none; padding: 0px; }");
+  mClearBtn->setToolTip(tr("Clear Field"));
+  mClearBtn->hide();
+
+  connect(mClearBtn, SIGNAL(clicked()), this, SLOT(clear()));
+  connect(this, SIGNAL(textChanged(const QString&)), this, SLOT(updateCloseButton(const QString&)));
+
+  int frameWidth = style()->pixelMetric(QStyle::PM_DefaultFrameWidth);
+
+  setStyleSheet(QString("QLineEdit { padding-right: %1px; } ").arg(mClearBtn->sizeHint().width() + frameWidth + 1));
+
+  QSize msz = minimumSizeHint();
+  setMinimumSize( qMax(msz.width(), mClearBtn->sizeHint().height() + frameWidth * 2 + 2)
+               , qMax(msz.height(), mClearBtn->sizeHint().height() + frameWidth * 2 + 2) );
+}
+
+LineEdit::~LineEdit()
+{}
+
+void LineEdit::resizeEvent(QResizeEvent *)
+{
+  QSize sz = mClearBtn->sizeHint();
+
+  int frameWidth = style()->pixelMetric(QStyle::PM_DefaultFrameWidth);
+
+  mClearBtn->move( rect().right() - frameWidth - sz.width()
+                , (rect().bottom() + 1 - sz.height()) / 2 );
+}
+
+void LineEdit::updateCloseButton(const QString& text)
+{
+  mClearBtn->setVisible(!text.isEmpty());
+}
---
+++ libs/common/LineEdit.h
@@ -0,0 +1,54 @@
+//
+//   This file is part of Filu.
+//
+//   Copyright (C) 2007, 2010  loh.tar@googlemail.com
+//
+//   Filu is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   Filu is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with Filu. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef LINEEDIT_H
+#define LINEEDIT_H
+
+#include <QLineEdit>
+
+/***********************************************************************
+*
+*   This is our QLineEdit with an extra clear button inside.
+*
+*   Thanks to Girish Ramakrishnan
+*   http://labs.qt.nokia.com/2007/06/06/lineedit-with-a-clear-button/
+*
+************************************************************************/
+
+class QToolButton;
+
+class LineEdit : public QLineEdit
+{
+  Q_OBJECT
+
+  public:
+                  LineEdit(QWidget *parent = 0);
+                 ~LineEdit();
+
+  protected:
+    void          resizeEvent(QResizeEvent *);
+
+  private slots:
+    void          updateCloseButton(const QString &text);
+
+  private:
+    QToolButton*  mClearBtn;
+};
+
+#endif
