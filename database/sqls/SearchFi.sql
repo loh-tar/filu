1d981daf50ef787383138fc0d76b83119e64eaa1
loh.tar <loh.tar@googlemail.com>
2012-12-10 21:09:49 +0100

    PerformerF: Change SearchFIWidget not to show NoMarkets

--- database/sqls/SearchFi.sql
+++ database/sqls/SearchFi.sql
@@ -21,9 +21,11 @@ SELECT f.fi_id, s.market_id, f.caption, t.caption, s.caption, m.caption
   WHERE f.ftype_id = t.ftype_id
     and f.fi_id = s.fi_id
     and s.market_id = m.market_id
-    and s.market_id != 0
+    and s.market_id > 1 -- Don't list NoMarket
     and (lower(f.caption) LIKE '%'|| lower(:name) ||'%'
-          or lower(s.caption) LIKE '%'|| lower(:name) ||'%')
+        or  EXISTS ( select s2.fi_id from :filu.symbol s2
+                       where lower(s2.caption) LIKE '%'|| lower(:name) ||'%'
+                         and s2.fi_id = s.fi_id
+                   )
+        )
     and lower(t.caption) LIKE '%'|| lower(:type)||'%'
-
-
