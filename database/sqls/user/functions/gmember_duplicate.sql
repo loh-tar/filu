b2ce4751e138a74282c1829403a685ccfc52d3da
loh.tar <loh.tar@googlemail.com>
2013-01-16 19:12:17 +0100

    Update copyright to 2013

--- database/sqls/user/functions/gmember_duplicate.sql
+++ database/sqls/user/functions/gmember_duplicate.sql
@@ -1,7 +1,7 @@
 --
 --   This file is part of Filu.
 --
---   Copyright (C) 2007, 2010, 2011, 2012  loh.tar@googlemail.com
+--   Copyright (C) 2007, 2010, 2011, 2012, 2013 loh.tar@googlemail.com
 --
 --   Filu is free software: you can redistribute it and/or modify
 --   it under the terms of the GNU General Public License as published by
