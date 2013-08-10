7a3ff354b4bde93d5178056c4d05b1b7fc9e5d68
loh.tar <loh.tar@googlemail.com>
2013-08-10 19:43:44 +0200

    CMake: Configure config files with @CMAKE_INSTALL_PREFIX@
    Great! Now is finitely the hard coded install path gone,
    and more better, without an ugly .in file

--- cmake/install-systemwide-config-file.cmake
+++ cmake/install-systemwide-config-file.cmake
@@ -2,14 +2,19 @@
 # Install systemwide settings file
 #
 
+configure_file(${FiluRoot_SOURCE_DIR}/doc/systemwide-settings-file.conf
+               ${FiluRoot_BINARY_DIR}
+               @ONLY
+)
+
 if(EXISTS "/etc/xdg/Filu.conf")
-  file(INSTALL "${FiluRoot_SOURCE_DIR}/doc/systemwide-settings-file.conf"
+  file(INSTALL "${FiluRoot_BINARY_DIR}/systemwide-settings-file.conf"
        DESTINATION "/etc/xdg"
        RENAME "Filu.conf.new")
   message("*\n*  NOTICE: /etc/xdg/Filu.conf installed as Filu.conf.new\n*")
 
 else()
-  file(INSTALL "${FiluRoot_SOURCE_DIR}/doc/systemwide-settings-file.conf"
+  file(INSTALL "${FiluRoot_BINARY_DIR}/systemwide-settings-file.conf"
        DESTINATION "/etc/xdg"
        RENAME "Filu.conf")
 
